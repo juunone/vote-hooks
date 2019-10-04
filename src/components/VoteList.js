66966239e71a3d88a05fb20f94f09a14f60662fc
juunone <juunone@gmail.com>
2019-10-04 14:40:46 +0900

    Modified porting votelist components

--- src/components/VoteList.js
+++ src/components/VoteList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as types from '../actions/ActionTypes'
@@ -11,32 +11,26 @@ const Section = loadable(() => import(/* webpackChunkName: "Section" */'./common
 const Card = loadable(() => import(/* webpackChunkName: "Card" */'./common/Card'))
 const Modal = loadable(() => import(/* webpackChunkName: "Modal" */'./common/Modal'))
 
+const VoteList = (props) => {
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = () => {
+    props.handleFetchData();
+  };
 
-class VoteList extends Component{
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount() {
-    this._fetchData();
-  }
-
-  _fetchData() {
-    this.props._handleFetchData();
-  }
-
-  _handleDelete = (onClose, data) => {
-    this.props._handleFetchData('DELETE', data);
+  const handleDelete = useCallback((onClose, data) => {
+    props.handleFetchData('DELETE', data);
     onClose();
-  }
+  });
 
-  _handleSave = (onClose, data, path) => {
-    this.props._handleFetchData('PUT', data, path);
+  const handleSave = useCallback((onClose, data, path) => {
+    props.handleFetchData('PUT', data, path);
     onClose();
-  }
+  });
 
-  _settingVote = (type, data) => {    
+  const settingVote = useCallback((type, data) => {    
     return( 
       confirmAlert({
         closeOnEscape: true,
@@ -47,16 +41,16 @@ class VoteList extends Component{
               type={type}
               data={data}
               onClose={onClose} 
-              handleSave={this._handleSave} 
-              handleDelete={this._handleDelete} 
+              handleSave={handleSave} 
+              handleDelete={handleDelete} 
             />
           )
         }      
       })
     )
-  }
+  }, [props.standingData])
 
-  _voting = (type, data) => {
+  const voting = useCallback((type, data) => {
     return( 
       confirmAlert({
         closeOnEscape: true,
@@ -67,16 +61,16 @@ class VoteList extends Component{
               type={type}
               data={data}
               onClose={onClose} 
-              handleSave={this._handleSave} 
-              handleDelete={this._handleDelete} 
+              handleSave={handleSave} 
+              handleDelete={handleDelete} 
             />
           )
         }      
       })
     )
-  }
+  }, [props.onGoingData])
 
-  _resultVote = (type, data) => {
+  const resultVote = useCallback((type, data) => {
     return( 
       confirmAlert({
         closeOnEscape: true,
@@ -87,69 +81,68 @@ class VoteList extends Component{
               type={type}
               data={data}
               onClose={onClose} 
-              handleDelete={this._handleDelete} 
+              handleDelete={handleDelete} 
             />
           )
         }      
       })
     )
-  }
+  }, [props.closedData])
 
-  _renderData(data, type) {
+  const renderData = (data, type) => {
+    console.log(data)
     const mappingData = data.map((v,i) => {
       return (
         <div key={i} className={'container__card'}>
-          <Card data={data[i]} type={type} settingVote={this._settingVote} voting={this._voting} resultVote={this._resultVote} />
+          <Card data={data[i]} type={type} settingVote={settingVote} voting={voting} resultVote={resultVote} />
         </div>
       )
     });
 
     return mappingData;
   }
-
-  render(){
-    const {standingData, onGoingData, closedData, error , loading} = this.props;
-    if (error) {
-      return <div className={'no-data'}>&#x1F6A8;네트워크 에러<br/><br/>로컬 환경에서 접속해주세요.</div>;
-    }
-
-    if (loading) {
-      return <div className={'no-data'}>Loading <FontAwesomeIcon icon={faSpinner} spin={true} /></div>;
-    }
-    
-    if(standingData.length || onGoingData.length || closedData.length) {
-      return(
-        <Section className={'container__section'}>        
-          {!!standingData.length && (
-            <div className={"container__row vote__row--standing"}>
-              <Title key='title' className="container__title">&#129304; 대기중인 투표</Title>
-              {this._renderData(standingData, 'standing')}
-            </div>
-          )}
-          {!!onGoingData.length && (
-            <div className={"container__row vote__row--ongoing"}>
-              <Title key='title' className="container__title">&#x1F525; 진행중인 투표</Title>
-              {this._renderData(onGoingData, 'ongoing')}
-            </div>
-          )}
-          {!!closedData.length && (
-            <div className={"container__row vote__row--closed"}>
-              <Title key='title' className="container__title">&#x2705; 종료된 투표</Title>
-              {this._renderData(closedData, 'closed')}
-            </div>
-          )}
-        </Section>
-      )
-    } else {
-      return(
-        <div className={'no-data'}>
-          <FontAwesomeIcon icon={faVoteYea} size={"2x"} color={"#0072ff"} />
-          <h3><b style={{color:'#0072ff'}}>선한 영향력</b><br />투표를 만들어주세요&#129304;</h3>
-        </div>
-      )
-    }
+  
+  const {standingData, onGoingData, closedData, error , loading} = props;
+  if (error) {
+    return <div className={'no-data'}>&#x1F6A8;네트워크 에러<br/><br/>로컬 환경에서 접속해주세요.</div>;
   }
-}
+
+  if (loading) {
+    return <div className={'no-data'}>Loading <FontAwesomeIcon icon={faSpinner} spin={true} /></div>;
+  }
+  
+  if(standingData.length || onGoingData.length || closedData.length) {
+    return(
+      <Section className={'container__section'}>        
+        {!!standingData.length && (
+          <div className={"container__row vote__row--standing"}>
+            <Title key='title' className="container__title">&#129304; 대기중인 투표</Title>
+            {renderData(standingData, 'standing')}
+          </div>
+        )}
+        {!!onGoingData.length && (
+          <div className={"container__row vote__row--ongoing"}>
+            <Title key='title' className="container__title">&#x1F525; 진행중인 투표</Title>
+            {renderData(onGoingData, 'ongoing')}
+          </div>
+        )}
+        {!!closedData.length && (
+          <div className={"container__row vote__row--closed"}>
+            <Title key='title' className="container__title">&#x2705; 종료된 투표</Title>
+            {renderData(closedData, 'closed')}
+          </div>
+        )}
+      </Section>
+    )
+  } else {
+    return(
+      <div className={'no-data'}>
+        <FontAwesomeIcon icon={faVoteYea} size={"2x"} color={"#0072ff"} />
+        <h3><b style={{color:'#0072ff'}}>선한 영향력</b><br />투표를 만들어주세요&#129304;</h3>
+      </div>
+    )
+  }
+};
 
 VoteList.propTypes = {
   standingData: PropTypes.array,
@@ -158,11 +151,11 @@ VoteList.propTypes = {
   data: PropTypes.array,
   error: PropTypes.string,
   loading: PropTypes.bool,
-  _handleFetchData: PropTypes.func
+  handleFetchData: PropTypes.func
 };
 
 const mapDispatchToProps = dispatch => ({
-  _handleFetchData: (method, data) => { dispatch({type: types.FETCH_DATA_START, payload: {method, data}}) },
+  handleFetchData: (method, data) => { dispatch({type: types.FETCH_DATA_START, payload: {method, data}}) },
 })
 
 const mapStateToProps = state => ({
